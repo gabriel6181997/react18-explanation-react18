d8c4bbe3fd19db7c10f7a5d0f54d2f0124657691
gabriel6181997 <gabriel6181997@gmail.com>
2024-07-19 18:50:31 +0900

    ✨ ReactQueryコンポーネントを実装済み

---
+++ src/components/ReactQuery.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+const fetchAlbums = async () => {
+  const result = await axios.get<Album[]>(
+    "https://jsonplaceholder.typicode.com/albums"
+  );
+  return result.data;
+};
+
+export const ReactQuery = () => {
+  const { isLoading, error, data } = useQuery<Album[]>({
+    queryKey: ["albums"],
+    queryFn: fetchAlbums,
+  });
+
+  if(error) return <p>エラーです！</p>
+  if(isLoading) return <p>ローディング中だよ〜</p>
+
+  return (
+    <div>
+      {data?.map((album) => (
+        <p key={album.id}>{album.title}</p>
+      ))}
+    </div>
+  );
+};
